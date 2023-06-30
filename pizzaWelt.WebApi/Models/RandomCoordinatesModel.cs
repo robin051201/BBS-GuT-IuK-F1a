b5f220c59b1c1d5a737cab4b8ed063860f4953ea
Robin Schu <Robin.Schu@merbag.lu>
2023-06-30 11:57:08 +0200

    init

---
+++ pizzaWelt.WebApi/Models/RandomCoordinatesModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace pizzaWelt.Models;
+
+
+
+public class Rootobject
+{
+    public Location[]? LocationSet { get; set; }
+}
+
+public class Location
+{
+    public string? CurrentLocation { get; set; }
+    public string? Latitude { get; set; }
+    public string? Longitude { get; set; }
+}
+
+
